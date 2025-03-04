af2e307df53b7cfeec1958196848796c55619a9d
djangid63 <jangiddevesh031@gmail.com>
2025-03-04 14:11:57 +0530

    refactor: remove console log from ViewMore component

--- Day_05-LogIn-Auth-Test-Revison/src/Components/ViewMore.jsx
+++ Day_05-LogIn-Auth-Test-Revison/src/Components/ViewMore.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const ViewMore = ({ product }) => {
-  console.log(product);
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4">
       <div className="bg-white rounded-lg p-6 max-w-2xl w-full max-h-[90vh] overflow-y-auto">
