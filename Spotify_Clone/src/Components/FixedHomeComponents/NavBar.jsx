6072f1f114ee61b1dd633c3837f712382ba01dab
djangid63 <jangiddevesh031@gmail.com>
2025-03-14 23:28:16 +0530

    feat: add react-icons for improved UI and update NavBar and HomePage layout

--- Spotify_Clone/src/Components/FixedHomeComponents/NavBar.jsx
+++ Spotify_Clone/src/Components/FixedHomeComponents/NavBar.jsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react'
+import { MdSearch } from "react-icons/md";
+import { FaRegBell } from "react-icons/fa";
 
 const NavBar = () => {
   const [search, setSearch] = useState('')
   return (
-    <nav>
-      <div className='m-4'>
-        <label htmlFor='search' className='sr-only' >Search</label>
-        <input
-          type='text'
-          id='search'
-          name='search'
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          placeholder='Artists, titles, podcasts'
-          className='px-6 py-3 rounded-xl border-2 border-purple-200 focus:outline-none focus:border-purple-400 w-full sm:w-80 shadow-sm'
-        ></input>
+    <nav  >
+      <div className='h-[100%] flex justify-between items-center my-[6px] mx-2 sm:m-3 sm:mx-6 '>
+        <div className='flex items-center border-2 border-purple-200 rounded-md focus-within:border-purple-400 bg-primary shadow-sm'>
+          <label htmlFor='search' className='sr-only'>Search</label>
+          <MdSearch className="mx-1 mt-1 text-gray-200" size={18} />
+          <input
+            type='text'
+            id='search'
+            name='search'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder='Artists, titles, podcasts...'
+            className='sm:w-80 w-32 md:px-4 md:py-2 px-[1px] py-[1px] focus:outline-none bg-transparent sm:placeholder:text-base placeholder:text-xs rounded-full'
+          />
+        </div>
+        <div className='flex justify-center items-center gap-5'>
+          <span><FaRegBell /></span>
+          <span className='flex items-center justify-center w-6 h-6 bg-primary rounded-full text-xs text-center'>DJ</span>
+        </div>
       </div>
     </nav>
   )
--- Spotify_Clone/src/Pages/HomePage.jsx
+++ Spotify_Clone/src/Pages/HomePage.jsx
@@ -4,18 +4,19 @@ import NavBar from '../Components/FixedHomeComponents/NavBar'
 const HomePage = () => {
   return (
     <section>
-      <div className="w-screen h-screen grid grid-cols-5 grid-rows-5">
+      <div className="w-screen h-screen grid grid-cols-5 grid-rows-16 sm:grid-rows-10 gap-0 ">
         {/* SideBar */}
-        <div className="row-span-4 bg-amber-200">1</div>
+        <div className="row-span-15 bg-amber-200">1</div>
 
         {/* NavBar */}
-        <div className="col-span-4"> <NavBar /> </div>
+        <div className="col-span-4 row-span-1 m-0 p-0"> <NavBar /> </div>
 
         {/* Main Content */}
-        <div className="col-span-4 row-span-3 bg-fuchsia-100">3</div>
-        
+        <div className="col-span-4 row-span-14 bg-fuchsia-800">3</div>
+
         {/* BottomBar */}
-        <div className="col-span-5 row-start-5 bg-red-700">4</div>
+        <div className=" col-span-5 row-start-16 bg-red-700">4</div>
+
       </div>
     </section>
   )
