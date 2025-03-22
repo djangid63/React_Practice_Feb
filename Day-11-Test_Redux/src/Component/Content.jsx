570847755b24babc2cdf2f68a7983314bbb83173
djangid63 <jangiddevesh031@gmail.com>
2025-03-22 13:39:21 +0530

    feat: enhance Content component with theme and layout toggle functionality

--- Day-11-Test_Redux/src/Component/Content.jsx
+++ Day-11-Test_Redux/src/Component/Content.jsx
@@ -4,16 +4,26 @@ import UseDarkMode from './UseDarkMode'
 
 
 const Content = () => {
-  const { isDark, handleApperance } = UseDarkMode()
-  console.log(handleApperance);
+  const { isDark, handleApperance, isFlex, handleFlex } = UseDarkMode()
+
   return (
     <div>
-
-      <button onClick={handleApperance} className={` h-10 w-50 border-2 border-gray-500 flex flex-col md:flex-row items-center justify-center gap-4 p-6 ${isDark ? "bg-black" : "bg-white"} shadow-md`}>
-        Change Theme
-      </button>
-      <div className={` h-10 w-50 border-2 border-gray-500 flex flex-col md:flex-row items-center justify-center gap-4 p-6 ${isDark ? "bg-black" : "bg-white"} shadow-md`}>
-
+      <div className='flex'>
+        <button onClick={handleApperance} className={` h-10 w-[50%] border-2 border-gray-500 flex flex-col md:flex-row items-center justify-center gap-4 p-6 ${isDark ? "bg-black" : "bg-white"} ${isDark ? "text-white" : "text-black"}  shadow-md`}>
+          {isDark ? "Light Mode" : "Dark Mode"}
+        </button>
+        <button onClick={handleFlex} className={` h-10 w-[50%] border-2 border-gray-500 flex flex-col md:flex-row items-center justify-center gap-4 p-6   ${isDark ? "bg-black" : "bg-white"} ${isDark ? "text-white" : "text-black"} shadow-md`}>
+          {isFlex ? "Row" : "Column"}
+        </button>
+      </div>
+      <div className={`h-screen w-screen  ${isFlex ? "flex" : "flex-col"} items-center jusc border-2 border-gray-500  gap-4 p-6 ${isDark ? "bg-black" : "bg-white"} shadow-md`}>
+        <div className='w-52 h-52 border-5 border-gray-400' ></div>
+        <div className='w-52 h-52 border-5 border-gray-400'></div>
+        <div className='w-52 h-52 border-5 border-gray-400'></div>
+        <div className='w-52 h-52 border-5 border-gray-400'></div>
+        <div className='w-52 h-52 border-5 border-gray-400'></div>
+        <div className='w-52 h-52 border-5 border-gray-400'></div>
+        <div className='w-52 h-52 border-5 border-gray-400'></div>
       </div>
     </div>
   )
--- Day-11-Test_Redux/src/Component/UseDarkMode.jsx
+++ Day-11-Test_Redux/src/Component/UseDarkMode.jsx
@@ -2,14 +2,21 @@ import React, { useState } from 'react'
 
 const UseDarkMode = () => {
   const [isDark, setDarkMode] = useState(false)
+  const [isFlex, setIsflex] = useState(false)
 
   const handleApperance = () => {
     setDarkMode(!isDark)
   }
+  const handleFlex = () => {
+    setIsflex(!isFlex)
+  }
 
   return {
     isDark,
-    handleApperance
+    handleApperance,
+    isFlex,
+    handleFlex
+
   }
 }
 
